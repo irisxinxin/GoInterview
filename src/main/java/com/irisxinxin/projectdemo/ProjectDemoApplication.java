473ffe288b75a9d744905a0219909c7b4da21afb
xin <xin@xindeMacBook-Air.local>
2020-12-09 16:27:31 +0800

    empty

---
+++ src/main/java/com/irisxinxin/projectdemo/ProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.irisxinxin.projectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDemoApplication {
+    //启动类
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDemoApplication.class, args);
+    }
+
+}
